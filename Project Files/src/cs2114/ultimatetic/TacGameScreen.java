1fcc48b1370d1773eb18eaadcc6040f6e7119824
Samuel Bahr <sfbahr@vt.edu>
2014-03-27 01:24:53 -0400

    created screen properly and modified manifest for tests

---
+++ Project Files/src/cs2114/ultimatetic/TacGameScreen.java
@@ -0,0 +1,29 @@
+package cs2114.ultimatetic;
+
+import sofia.app.ShapeScreen;
+import sofia.graphics.*;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TacGameScreen
+    extends ShapeScreen
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tacgamescreen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tac_game_screen, menu);
+        return true;
+    }
+
+}
