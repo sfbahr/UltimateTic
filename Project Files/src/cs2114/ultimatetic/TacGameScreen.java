46d80f54378dc86b68180d26b0ee93fb630eb6d1
Samuel Bahr <sfbahr@vt.edu>
2014-04-02 01:19:39 -0400

    The lines for the board are now displayed
    These are not functional lines, they are only for the GUI. The actual
    cells have yet to be implemented so that they can display various states.

--- Project Files/src/cs2114/ultimatetic/TacGameScreen.java
+++ Project Files/src/cs2114/ultimatetic/TacGameScreen.java
@@ -19,7 +19,14 @@ public class TacGameScreen
     extends ShapeScreen
 {
     // ~ Fields ................................................................
-    private Grid grid; //the array of nine tac boards
+    private Grid grid; //the grid of nine tac boards
+    private final int gridPad = 20; //The padding on the grid
+    private final int boardPad = 15;
+    private final int gridLineWidth = 10;
+    private final int boardLineWidth = 5;
+    private float gridSize;
+    private float boardSize;
+    private float cellSize;
 
     // ~ Methods ...............................................................
 
@@ -28,8 +35,57 @@ public class TacGameScreen
      */
     public void initialize()
     {
+        grid = new Grid();
+        gridSize = Math.min(getWidth(), getHeight()) - 2 * gridPad;
+        boardSize = gridSize / 3 - 2 * boardPad;
+        cellSize = boardSize / 3;
 
+      //Create the Grid's lines
+        for (int i = 1; i <= 2; i++)
+        {
+            LineShape verticalLine = new LineShape(gridPad + gridSize / 3 * i,
+                gridPad, gridPad + gridSize / 3 * i, gridPad + gridSize);
+            LineShape horizontalLine = new LineShape(gridPad,
+                gridPad + gridSize / 3 * i, gridPad + gridSize,
+                gridPad + gridSize / 3 * i);
+            verticalLine.setColor(Color.black);
+            horizontalLine.setColor(Color.black);
+            verticalLine.setStrokeWidth(gridLineWidth);
+            horizontalLine.setStrokeWidth(gridLineWidth);
+            add(verticalLine);
+            add(horizontalLine);
+        }
 
+        //Create each board's lines
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                for (int linNum = 1; linNum <= 2; linNum++)
+                {
+                    float xVert = gridPad + gridSize / 3 * i + boardPad +
+                        boardSize / 3 * linNum;
+                    float yVertTop = gridPad + gridSize / 3 * j + boardPad;
+                    float yVertBottom = yVertTop + boardSize;
+                    LineShape verticalLine = new LineShape(xVert, yVertTop,
+                        xVert, yVertBottom);
+
+                    float yHorz = gridPad + gridSize / 3 * j + boardPad +
+                        boardSize / 3 * linNum;
+                    float xHorzLeft = gridPad + gridSize / 3 * i + boardPad;
+                    float xHorzRight = xHorzLeft + boardSize;
+                    LineShape horizontalLine = new LineShape(xHorzLeft, yHorz,
+                        xHorzRight, yHorz);
+
+                    verticalLine.setColor(Color.black);
+                    horizontalLine.setColor(Color.black);
+                    verticalLine.setStrokeWidth(boardLineWidth);
+                    horizontalLine.setStrokeWidth(boardLineWidth);
+                    add(verticalLine);
+                    add(horizontalLine);
+                }
+            }
+        }
     }
 
     /**
