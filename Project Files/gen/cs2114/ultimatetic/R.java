d150be77a4959a4c0499a9f9deda79c51a17f53d
Samuel Bahr <sfbahr@vt.edu>
2014-04-14 20:06:14 -0400

    Added undo button and removed settings button
    The settings button was doing nothing so I removed its useless self.
    The undo button is as of now nonfunctional but it shall be once the stack
    is implemented in the model tracking all of the moves.

--- Project Files/gen/cs2114/ultimatetic/R.java
+++ Project Files/gen/cs2114/ultimatetic/R.java
@@ -22,11 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_refresh=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_undo=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_refresh=0x7f080001;
-        public static final int action_settings=0x7f080002;
+        public static final int action_refresh=0x7f080002;
+        public static final int action_undo=0x7f080001;
         public static final int shapeView=0x7f080000;
     }
     public static final class layout {
@@ -38,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_refresh=0x7f040004;
         public static final int action_settings=0x7f040002;
+        public static final int action_undo=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040003;
         public static final int title_activity_tac_game_screen=0x7f040001;
